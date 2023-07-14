81f9612c6627b3a40e415f29e083243b4ea3c3fd
jawad <jawadhakimee1401@gmail.com>
2023-07-14 20:30:13 +0430

    Using sizeof

--- learn.cpp
+++ learn.cpp
@@ -3,12 +3,6 @@
 using namespace std;
 
 int main(){
-    float a,b,c, sum,ave;
-
-
-    cin >>a>>b>>c;
-    sum = a+b+c;
-    ave = sum/3;
-    cout<<"Sum = "<<sum<<'\t'<<"Ave = "<< ave <<endl;
-
+int x;
+cout << sizeof(x);
 }
